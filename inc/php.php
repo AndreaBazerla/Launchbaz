f3c1e49d168730f083f466f570d37812f5b82de4
Andrea Bazerla <andrea.bazerla@gmail.com>
2016-02-28 16:58:44 +0100

    Created get_mp3() to check if sounds exist

--- inc/php.php
+++ inc/php.php
@@ -2,6 +2,7 @@
 
 	$dir = 1;
 	$kit = 1;
+	$num = 3;
 
 	function get_dir() {
 		echo 1;
@@ -17,6 +18,23 @@
 		echo "<div id='k3'>3</div>";
 		echo "<div id='k4'>4</div>";
 		echo "<div id='k5'>5</div>";
+	}
+
+	function get_mp3() {
+		global $dir,$kit,$num,$mp3;
+		$mp3 = array();
+		for($i=0;$i<$num+1;$i++)
+			$mp3[$i] = array();
+		for($i=1;$i<$num+1;$i++) {
+			for($j=65;$j<91;$j++) {
+				if(file_exists($_SERVER['DOCUMENT_ROOT']."/wp-content/themes/launchbaz/kit/".$dir."/".$i."/".chr($j).".mp3")) {
+					$mp3[$i][$j] = 1;
+				} else {
+					$mp3[$i][$j] = 0;
+				}
+			}
+		}
+		echo json_encode($mp3);
 	}
 
 ?>
